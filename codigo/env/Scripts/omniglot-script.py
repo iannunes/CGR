d88c52722e31c88fc5788c4ead5405e5d1336475
Ian Nunes <iannunes@msn.com>
2021-06-23 17:40:19 -0300

    primeiro commit

---
+++ codigo/env/Scripts/omniglot-script.py
@@ -0,0 +1,12 @@
+#!D:\repos\CGR\codigo\env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'omniglot==0.1.0','console_scripts','omniglot'
+__requires__ = 'omniglot==0.1.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('omniglot==0.1.0', 'console_scripts', 'omniglot')()
+    )
